797a2d102fe51a7d590f411c02c74333b4d2f874
Hyominn <kris42@163.com>
2020-09-05 23:10:49 +0800

    feat(Adapter): IDuck

---
+++ Adapter/TurkeyAdapter.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Adapter
+{
+    public class TurkeyAdapter : IDuck
+    {
+        private readonly ITurkey _turkey;
+
+        public TurkeyAdapter(ITurkey turkey)
+        {
+            _turkey = turkey;
+        }
+
+        public void Quack()
+        {
+            _turkey.Gobble();
+        }
+
+        public void Fly()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                _turkey.Fly();
+                Console.WriteLine("Resting..");
+            }
+        }
+    }
+}
